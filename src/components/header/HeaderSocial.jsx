a1f9f5c4f456e75ce72004800af478a978f2738f
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-02-15 23:39:08 +0300

    testing links

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -7,6 +7,8 @@ const HeaderSocials = () => {
     <div className="header__socials">
       <a href="https://www.linkedin.com/in/timothy-mureithi/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
       <a href="https://github.com/timothymureithi" target="_blank" rel="noreferrer" ><FaGithub /></a>
+            <a href="https://github.com/timothymureithi" target="_blank" rel="noreferrer" ><FaGithub /></a>
+
     </div>
   )
 }
