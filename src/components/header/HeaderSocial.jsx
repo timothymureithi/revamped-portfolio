41590376489027ef76e5a1d3c13e2d90389fac4e
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-02-17 12:38:01 +0300

    header section testing links

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -4,12 +4,12 @@ import { FaGithub } from 'react-icons/fa';
 
 const HeaderSocials = () => {
   return (
-    // <div className="header__socials">
-    //   <a href="https://www.linkedin.com/in/timothy-mureithi/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
-    //   <a href="https://github.com/timothymureithi" target="_blank" rel="noreferrer" ><FaGithub /></a>
-    //         <a href="https://github.com/timothymureithi" target="_blank" rel="noreferrer" ><FaGithub /></a>
+    <div className="header__socials">
+      <a href="https://www.linkedin.com/in/timothy-mureithi/" target="_blank" rel="noreferrer" ><BsLinkedin /></a>
+      <a href="https://github.com/timothymureithi" target="_blank" rel="noreferrer" ><FaGithub /></a>
+            <a href="https://github.com/timothymureithi" target="_blank" rel="noreferrer" ><FaGithub /></a>
 
-    // </div>
+    </div>
   )
 }
 
