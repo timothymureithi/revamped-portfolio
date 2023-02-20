522cf5c8ad0697fdfc23eafe956713e0a5a67592
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-02-20 23:53:45 +0300

    redo header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,18 +5,7 @@ import './header.css';
 
 const Header = () => {
     return (
-      <header id="home">
-        <div className="container header__container">
-          <h5>Hello I'm</h5>
-          <h1>Timothy Mureithi</h1>
-          <h5 className="text-light">Full-stack Developer</h5>
-          <CTA />
-          <a href="#contact" className="scroll__down">
-            Scroll Down
-          </a>
-          <HeaderSocials />
-        </div>
-      </header>
+     
     );
   };
 export default Header;
