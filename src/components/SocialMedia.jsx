8804d4fbd522653b4a6392add267dd23c704232f
Timothy Mureithi <timothy.mureithi@student.moringaschool.com>
2023-02-26 22:06:43 +0300

    social media links

---
+++ src/components/SocialMedia.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+const SocialMedia = () => (
+    <div className="app__social app__navbar-social">
+        <div>
+            <a
+                href="https://github.com/timothymureithi"
+                style={{ height: "20px" }}
+                target="_blank">
+                <GitHubIcon />
+            </a>
+        </div>
+        <div>
+            <a
+                href="https://www.linkedin.com/in/timothy-mureithi/"
+                style={{ height: "20px" }}
+                target="_blank"
+            >
+                <LinkedInIcon />
+            </a>
+        </div>
+    </div>
+);
+
+export default SocialMedia;
